01840785253204563534f354350c5428c27bcacf
Akash M Nandan <akash003nandan@gmail.com>
2025-08-13 09:23:09 +0530

    importing THREE and //steps to do

---
+++ basic/main.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three'
+
+//1. create a scene
+
+//2. Add the camera
+
+//3. Create and add a cube object
+
+//4. Add lightning
+
+//5. Set up the renderer
+
+//6.Animate the scene
